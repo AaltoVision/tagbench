2259acc6684ff568867760530f0426e16c739b8e
Janne Gröndahl <janne.grondahl@aalto.fi>
2021-02-23 19:02:44 +0200

    Use cxxopts to read settings instead of hardcoded values

--- src/input_data_preprocessor.cpp
+++ src/input_data_preprocessor.cpp
@@ -1,4 +1,3 @@
-#include <nlohmann/json.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -13,6 +12,8 @@ namespace fs = std::filesystem;
 namespace fs = std::experimental::filesystem;
 #endif
 
+#include <nlohmann/json.hpp>
+#include <cxxopts.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgproc/imgproc_c.h>
@@ -20,14 +21,6 @@ namespace fs = std::experimental::filesystem;
 #include "detect_markers.h"
 
 using json = nlohmann::json;
-
-static auto timing = [](auto const& f) {
-    auto start = std::chrono::steady_clock::now();
-    f();
-    auto end = std::chrono::steady_clock::now();
-    auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() * 1e-3f;
-    return dt;
-};
 
 // hacky, TODO some better way to contain the frame<->index info
 int parse_frame_number_from_path(fs::path const& path)
@@ -37,7 +30,7 @@ int parse_frame_number_from_path(fs::path const& path)
 
 
 // Prepare input data from given directory into .jsonl that 'tagbench' program expects
-// Output in jsonl format:
+// Example output in jsonl format:
 //
 //      {
 //          "frameIndex": 1,
@@ -49,27 +42,62 @@ int parse_frame_number_from_path(fs::path const& path)
 //
 int main(int argc, char* argv[])
 {
-    if (argc < 2)
+    auto input_dir_option = std::string{};
+    auto pose_data_name = std::string{};
+    auto output_file_path = std::string{};
+    auto image_downscale_factor = 1;
+
+    cxxopts::Options options(argv[0], "");
+    options.add_options()
+        ("i,input", "Path to the input data directory", cxxopts::value<std::string>(input_dir_option))
+        ("n,pose_data_name", "Name of pose data in the input", cxxopts::value<std::string>(pose_data_name))
+        ("m,image_downscale_factor", "Image downscaling factor", cxxopts::value<int>(image_downscale_factor)->default_value("1"))
+        ("o,output", "Output path (.jsonl file) (default: stdout)", cxxopts::value<std::string>(output_file_path))
+        ("h,help", "Show this help message")
+        ;
+
+    auto parsed_args = options.parse(argc, argv);
+    if (parsed_args.count("help"))
     {
-        std::cerr << "Missing input data path, exiting" << std::endl;
+        std::cout << options.help() << std::endl;
+        return 0;
+    }
+    if (parsed_args.count("input") == 0)
+    {
+        std::cerr << "Missing input data path argument." << std::endl;
+        std::cerr << options.help() << std::endl;
+        std::cerr << "See README.md for more instructions." << std::endl;
+        return 1;
+    }
+    if (parsed_args.count("pose_data_name") == 0)
+    {
+        std::cerr << "Missing pose data name." << std::endl;
+        std::cerr << options.help() << std::endl;
+        std::cerr << "See README.md for more instructions." << std::endl;
         return 1;
     }
 
     // Write to given file path, otherwise to stdout
     FILE* out = stdout;
-    if (argc > 2)
+    if (parsed_args.count("output"))
     {
-        out = fopen(argv[2], "w");
+        out = std::fopen(output_file_path.c_str(), "w");
         if (!out)
         {
             std::cerr << "Failed to open output file" << std::endl;
-            return 2;
+            return 1;
         }
     }
 
-    auto const input_dir = fs::path{ argv[1] };
+    auto const input_dir = fs::path{ input_dir_option };
     auto const input_frames_dir = input_dir / "frames";
-    auto input_sensor_readings = std::ifstream{ input_dir / "data.jsonl" };
+    auto const input_sensor_readings_path = input_dir / "data.jsonl";
+    auto input_sensor_readings = std::ifstream{ input_sensor_readings_path };
+    if (!input_sensor_readings)
+    {
+        std::fprintf(stderr, "Failed to open input file (does it exist?): %s", input_sensor_readings_path.string().c_str());
+        return 1;
+    }
 
     // Read in frame paths with their indices
     auto frame_paths = std::map<int, fs::path>{};
@@ -84,7 +112,7 @@ int main(int argc, char* argv[])
     {
         std::printf("Failed to parse frame numbers\n");
         std::printf("%s\n", e.what());
-        return 3;
+        return 1;
     }
 
     // Parse time->frame_index mapping
@@ -109,7 +137,7 @@ int main(int argc, char* argv[])
     {
         auto line_json = json::parse(line);
 
-        if (line_json.contains("arcore"))
+        if (line_json.contains(pose_data_name))
         {
             auto const time = line_json["time"].get<float>();
             auto const frame_index_it = time_to_frame_index.find(time);
@@ -118,7 +146,7 @@ int main(int argc, char* argv[])
                 auto const frame_index = frame_index_it->second;
                 if (camera_parameters[frame_index].is_null())
                     camera_parameters[frame_index] = json::object();
-                camera_parameters[frame_index]["cameraExtrinsics"] = line_json["arcore"];
+                camera_parameters[frame_index]["cameraExtrinsics"] = line_json[pose_data_name];
             }
         }
         if (line_json.contains("frames"))
@@ -150,14 +178,17 @@ int main(int argc, char* argv[])
 
             auto frame = cv::imread(frame_path.string());
             auto scaled_frame = cv::Mat{};
-            cv::resize(frame, scaled_frame, frame.size() / 2);
+            cv::resize(frame, scaled_frame, frame.size() / image_downscale_factor);
             j["markers"] = detect_markers(scaled_frame);
+
+            // Useful for flipping y-values in tagbench
+            j["frameHeight"] = frame.size().height;
 
             fprintf(out, "%s\n", j.dump().c_str());
         }
     }
 
 
-    if (out && (out != stdout)) fclose(out);
+    if (out && (out != stdout)) std::fclose(out);
 }
 
